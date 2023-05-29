ff2056855a4b73564970dd7dea814d26abe3d8da
H0mage <john2117@live.com>
2023-05-29 04:49:14 -0700

    Initial Setup, Form submits values entered

---
+++ main.js
@@ -0,0 +1,48 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const url = require("url");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
+
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+ipcMain.on("form:submit", function (event, formValues) {
+  // Access the form values here and perform desired actions
+  console.log(formValues);
+  // You can send the values to the main process or perform any other operations
+});
