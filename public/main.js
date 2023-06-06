280c7917888df29e9e1606a82d3ff637806790cf
H0mage <john2117@live.com>
2023-06-05 21:42:25 -0700

    Added the collage implemmentation for 5 and 6 images

--- public/main.js
+++ public/main.js
@@ -19,7 +19,7 @@ async function compositeImages(display, imagePaths) {
   const collageNumber = imagePaths.length;
 
   if (collageNumber === 2) {
-    const img_1_width = Math.floor((display.width / 10) * 6);
+    const img_1_width = Math.floor((display.width / 100) * 60);
     const img_2_width = display.width - img_1_width;
     try {
       const image1 = await sharp(imagePaths[0])
@@ -52,11 +52,55 @@ async function compositeImages(display, imagePaths) {
     } catch (error) {
       console.log(error);
     }
-  } else if (collageNumber === 4) {
-    const section_1_width = Math.floor((display.width / 10) * 6);
+  } else if (collageNumber === 3) {
+    const section_1_width = Math.floor((display.width / 100) * 57);
     const section_2_width = display.width - section_1_width;
-    const img_1_height = Math.floor((display.height / 100) * 40);
-    const img_1_width = Math.floor((section_1_width / 100) * 55);
+    const img_2_height = Math.floor((display.height / 100) * 55);
+    const img_3_height = display.height - img_2_height;
+    console.log(section_1_width, section_2_width, display.width);
+
+    try {
+      const image1 = await sharp(imagePaths[0])
+        .resize({
+          width: section_1_width,
+          height: display.height,
+        })
+        .toBuffer();
+      const image2 = await sharp(imagePaths[1])
+        .resize({
+          width: section_2_width,
+          height: img_2_height,
+        })
+        .toBuffer();
+      const image3 = await sharp(imagePaths[2])
+        .resize({
+          width: section_2_width,
+          height: img_3_height,
+        })
+        .toBuffer();
+
+      await sharp({
+        create: {
+          width: display.width,
+          height: display.height,
+          channels: 3,
+          background: "#000000",
+        },
+      })
+        .composite([
+          { input: image1, top: 0, left: 0 },
+          { input: image2, top: 0, left: section_1_width },
+          { input: image3, top: img_2_height, left: section_1_width },
+        ])
+        .toFile("test.png");
+    } catch (error) {
+      console.log(error);
+    }
+  } else if (collageNumber === 4) {
+    const section_1_width = Math.floor((display.width / 10) * 59);
+    const section_2_width = display.width - section_1_width;
+    const img_1_height = Math.floor((display.height / 100) * 38);
+    const img_1_width = Math.floor((section_1_width / 100) * 50);
     const img_2_width = section_1_width - img_1_width;
     const img_3_height = display.height - img_1_height;
     console.log(section_1_width, section_2_width, display.width);
@@ -105,30 +149,53 @@ async function compositeImages(display, imagePaths) {
     } catch (error) {
       console.log(error);
     }
-  } else if (collageNumber === 3) {
-    const section_1_width = Math.floor((display.width / 10) * 6);
+  } else if (collageNumber === 6) {
+    const section_1_width = Math.floor((display.width / 100) * 54);
     const section_2_width = display.width - section_1_width;
-    const img_2_height = Math.floor((display.height / 100) * 55);
-    const img_3_height = display.height - img_2_height;
-    console.log(section_1_width, section_2_width, display.width);
+    const section_3_height = Math.floor((display.height / 100) * 45);
+    const section_4_height = Math.floor((display.height / 100) * 42);
+    const img_1_width = Math.floor((section_1_width / 100) * 35);
+    const img_2_width = section_1_width - img_1_width;
+    const img_3_height = display.height - section_3_height;
+    const img_4_height = display.height - section_4_height;
+    const img_5_width = Math.floor((section_2_width / 100) * 56);
+    const img_6_width = section_2_width - img_5_width;
 
     try {
       const image1 = await sharp(imagePaths[0])
         .resize({
-          width: section_1_width,
-          height: display.height,
+          width: img_1_width,
+          height: section_3_height,
         })
         .toBuffer();
       const image2 = await sharp(imagePaths[1])
         .resize({
-          width: section_2_width,
-          height: img_2_height,
+          width: img_2_width,
+          height: section_3_height,
         })
         .toBuffer();
       const image3 = await sharp(imagePaths[2])
         .resize({
-          width: section_2_width,
+          width: section_1_width,
           height: img_3_height,
+        })
+        .toBuffer();
+      const image4 = await sharp(imagePaths[3])
+        .resize({
+          width: section_2_width,
+          height: img_4_height,
+        })
+        .toBuffer();
+      const image5 = await sharp(imagePaths[4])
+        .resize({
+          width: img_5_width,
+          height: section_4_height,
+        })
+        .toBuffer();
+      const image6 = await sharp(imagePaths[5])
+        .resize({
+          width: img_6_width,
+          height: section_4_height,
         })
         .toBuffer();
 
@@ -142,8 +209,76 @@ async function compositeImages(display, imagePaths) {
       })
         .composite([
           { input: image1, top: 0, left: 0 },
-          { input: image2, top: 0, left: section_1_width },
-          { input: image3, top: img_2_height, left: section_1_width },
+          { input: image2, top: 0, left: img_1_width },
+          { input: image3, top: section_3_height, left: 0 },
+          { input: image4, top: 0, left: section_1_width },
+          { input: image5, top: img_4_height, left: section_1_width },
+          {
+            input: image6,
+            top: img_4_height,
+            left: section_1_width + img_5_width,
+          },
+        ])
+        .toFile("test.png");
+    } catch (error) {
+      console.log(error);
+    }
+  } else if (collageNumber === 5) {
+    const section_1_width = Math.floor((display.width / 100) * 57);
+    const section_2_width = display.width - section_1_width;
+    const section_3_height = Math.floor((display.height / 100) * 43);
+    const section_3_width = Math.floor(section_1_width / 2);
+    const img_3_height = display.height - section_3_height;
+    const img_4_height = Math.floor((display.height / 100) * 54);
+    const img_5_height = display.height - img_4_height;
+    console.log(section_1_width, section_2_width, display.width);
+
+    try {
+      const image1 = await sharp(imagePaths[0])
+        .resize({
+          width: section_3_width,
+          height: section_3_height,
+        })
+        .toBuffer();
+      const image2 = await sharp(imagePaths[1])
+        .resize({
+          width: section_3_width,
+          height: section_3_height,
+        })
+        .toBuffer();
+      const image3 = await sharp(imagePaths[2])
+        .resize({
+          width: section_1_width,
+          height: img_3_height,
+        })
+        .toBuffer();
+      const image4 = await sharp(imagePaths[3])
+        .resize({
+          width: section_2_width,
+          height: img_4_height,
+        })
+        .toBuffer();
+      const image5 = await sharp(imagePaths[4])
+        .resize({
+          width: section_2_width,
+          height: img_5_height,
+        })
+        .toBuffer();
+
+      await sharp({
+        create: {
+          width: display.width,
+          height: display.height,
+          channels: 3,
+          background: "#000000",
+        },
+      })
+        .composite([
+          { input: image1, top: 0, left: 0 },
+          { input: image2, top: 0, left: section_3_width },
+          { input: image3, top: section_3_height, left: 0 },
+          { input: image4, top: 0, left: section_1_width },
+          { input: image5, top: img_4_height, left: section_1_width },
         ])
         .toFile("test.png");
     } catch (error) {
@@ -222,7 +357,7 @@ ipcMain.on("papetron:start", function (event) {
   console.log(displays);
   console.log(directories);
   fs.readdir(directories[0], async (err, files) => {
-    let collageNumber = 4; //CHANGE THIS TO 1 AFTER DONE
+    let collageNumber = 6; //CHANGE THIS TO 1 AFTER DONE
 
     if (isCollage) {
       collageNumber = chooseRandom(1, 6);
